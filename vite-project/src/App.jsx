ad9df2e7ba5c9b1f9623b081090a30013b3d7941
Duc Thai <ducthai060501@gmail.com>
2022-09-18 23:04:45 +0300

    handle change

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,25 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "../dist/output.css";
-
+import { NavBar  } from "./components";
+import {Welcome} from "./components"
+import {Footer} from "./components";
+import {Services} from "./components";
+import {Transaction} from "./components";
 const App = ( ) =>  {
   const [count, setCount] = useState(0);
 
   return (
-  <h1 className="text-3xl font-bold underline">Hello world!</h1>);
+  <div className="min-h-screen">
+    <div className="gradient-bg-welcome">
+      <NavBar/>
+      <Welcome /> 
+    </div>
+    <Services />
+    <Transaction /> 
+    <Footer /> 
+  </div>
+  )
 }
 
 export default App;
---
+++ vite-project/src/components/NavBar.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { HiMenuAlt4 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+import logo3 from "../images/logo3.png";
+import { useState } from "react";
+
+const NavBArItem = ({ title, classProps }) => {
+  return <li className={`mx-4 cursor-pointer ${classProps}`}>{title}</li>;
+};
+
+const NavBar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const I_ARRAY = ["Market", "Exchange", "Tutorials", "Wallets"];
+  return (
+    <nav className="w-full flex md:justify-center justify-between items-center p-4">
+      <div className="md:flex-[0.5] flex-initial justify-center items-center">
+        <img src={logo3} alt="logo" className="w-32 cursor-pointer" />
+      </div>
+      <ul className="text-white md:flex hidden list-none list-none flex-row justify-between items-center flex-initial">
+        {I_ARRAY.map((item, index) => (
+          <NavBArItem key={item + index} title={item} />
+        ))}
+        <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
+          Login
+        </li>
+      </ul>
+      <div className="flex relative">
+        {toggleMenu ? (
+          <AiOutlineClose
+            fontSize={28}
+            className="text-white md:hidden cursor-pointer"
+            onClick={() => {
+              setToggleMenu(false);
+            }}
+          />
+        ) : (
+          <HiMenuAlt4
+            fontSize={28}
+            className="text-white md:hidden cursor-pointer"
+            onClick={() => {
+              setToggleMenu(true);
+            }}
+          />
+        )}
+        {toggleMenu && (
+          <ul className="z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in">
+            <li className="text-xl w-full my-2">
+              <AiOutlineClose onClick={() => setToggleMenu(false)} />
+            </li>
+            {I_ARRAY.map((item, index) => (
+              <NavBArItem
+                key={item + index}
+                title={item}
+                classProps="my-2 text-lg"
+              />
+            ))}
+          </ul>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
